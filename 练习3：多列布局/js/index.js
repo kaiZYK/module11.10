2082dfd088459c1da891a4de873365e1979bbd73
xiaokai <1020849724@qq.com>
2022-11-10 14:44:04 +0800

    练习3：多列布局完成

--- 练习3：多列布局/js/index.js
+++ 练习3：多列布局/js/index.js
@@ -3,7 +3,7 @@ let html = document.documentElement;
 function setRem() {
   let ui_w = 375;
   let cl_w = document.documentElement.clientWidth || document.body.clientWidth;
-  cl_w = cl_w < 250 ? 250 : cl_w;
+  cl_w = cl_w < 340 ? 340 : cl_w;
   cl_w = cl_w > 800 ? 800 : cl_w;
   html.style.fontSize = (cl_w / ui_w) * 10 + "px";
 }
